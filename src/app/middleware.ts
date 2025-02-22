dbab8665d0a9fe0adde292e76cfb5e60df5c6692
atoms19 <vishalvdevan@gmail.com>
2025-02-22 13:13:56 +0530

    imporoved auth boilerplate

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,13 +1,17 @@
-import {auth} from "@/auth"
-
+import { auth } from "@/auth";
+//import { getSessionDoctor } from "./lib/utils";
 
 export default auth(async (req) => {
-    if (!req.auth && req.nextUrl.pathname !== "/login") {
-      const newUrl = new URL("/login", req.nextUrl.origin)
-      return Response.redirect(newUrl)
+  console.log("redired")
+   if (!req.auth && req.nextUrl.pathname !== "/login") {
+      const newUrl = new URL("/login", req.nextUrl.origin);
+      return Response.redirect(newUrl);
+   }else{
+      //logic to verify session
+      //redirect accordingly
+   }
+});
 
-    }
-  
-})
-
-
+export const config = {
+   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+};
